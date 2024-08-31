516a298944f0347763da24953a482247680b940c
ibtissam09 <ibtissam.makdoun@gmail.com>
2024-09-01 00:51:08 +0200

    troubleshouting dockerfile

--- 03/02/app.py
+++ 03/02/app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 import pandas as pd
 import numpy as np
+import html
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # Example of using pandas and numpy in your Flask app
+    # Exemple d'utilisation de pandas et numpy dans votre application Flask
     data = pd.DataFrame(np.random.randn(5, 3), columns=list('ABC'))
-    return f'Hello, World!<br><br>Here is some sample data:<br>{data.to_html()}'
+    # Échapper les caractères HTML pour éviter les attaques XSS
+    data_html = html.escape(data.to_html())
+    # Formater le message avec le contenu HTML
+    message = f'Hello, World!<br><br>Voici quelques données d'exemple :<br>{data_html}'
+    return message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
